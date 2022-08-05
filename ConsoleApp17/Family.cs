e7628d164f775e6a28b55cf2d0e570d08b5da764
Dan6ladi <dan6ladi@gmail.com>
2022-08-05 09:43:26 +0100

    Add project files.

---
+++ ConsoleApp17/Family.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    // this is an abstract class
+    internal abstract class Family
+    {
+        // father and role does not return any value
+        public abstract void father();
+        public void role() 
+        {
+            Console.WriteLine("Father is: Head of the family");
+        }
+    }
+    //This class was inherited from the class Family
+    class Son : Family 
+    {
+        public override void father()
+        {
+           Console.WriteLine("Son is: first child");
+        }
+    }
+
+}
---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp17
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Son myson = new Son();
+            myson.father();
+            myson.role();
+        }
+    }
+}
